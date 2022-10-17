ee2e2a89295035965e28d78af35b0719dee33db0
Ivan Kornilov <ivan.kornilov@selerant.com>
2022-10-17 05:06:00 +0300

    initial commit

---
+++ HelloWorldCICD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloWorldCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
