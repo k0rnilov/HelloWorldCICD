e98c0b43af3e5493c777749a041584fd144abeaf
Ivan Kornilov <ivan.kornilov@selerant.com>
2022-11-18 18:48:14 +0200

    added new implementation

--- HelloWorldCICD/Program.cs
+++ HelloWorldCICD/Program.cs
@@ -27,6 +27,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+// change
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
